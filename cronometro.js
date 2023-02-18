09a514d8da378646e8d0d4a54b744efbd4cf5623
Daniel Vitor <danielvitorlog221@gmail.com>
2023-02-18 09:46:22 -0300

    Cronometro

--- cronometro.js
+++ cronometro.js
@@ -1,4 +1,14 @@
-let tempo = 100
+let tempo = 0
+var nivel_tempo = window.location.search
+nivel_tempo = nivel.replace('?', '')
+
+if (nivel_tempo === 'facil') {
+    tempo = 30
+} else if (nivel === 'medio') {
+    tempo = 60
+} else if (nivel === 'dificil') {
+    tempo = 60
+}
 
 const cronometro = setInterval(function() {
     tempo -= 1
